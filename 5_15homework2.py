7271bcc9a9d686fa6cf3fc4891a40487ea1cd20a
Kyoko-N <65456318+Kyoko-N@users.noreply.github.com>
2020-05-17 17:20:33 +0900

    Update 5_15homework2.py

--- 5_15homework2.py
+++ 5_15homework2.py
@@ -1,3 +1,5 @@
+## the program for game- I can haz wordz [make 16 alphabets be words 'highest score']
+
 from collections import defaultdict
 
 valueForLetter = [1,1,2,1,1,2,1,2,1,3,3,2,2,1,1,2,3,1,1,1,1,2,2,3,2,3]
