08e4a1452d17a2138e037e6c32048933c81e7e85
Kyoko-N <65456318+Kyoko-N@users.noreply.github.com>
2020-05-20 13:44:33 +0900

    Update 5_15homework2.py

--- 5_15homework2.py
+++ 5_15homework2.py
@@ -2,7 +2,8 @@
 
 from collections import defaultdict
 
-valueForLetter = [1,1,2,1,1,2,1,2,1,3,3,2,2,1,1,2,3,1,1,1,1,2,2,3,2,3]
+valueForLetter = [1, 1, 2, 1, 1, 2, 1, 2, 1, 3, 3, 2, 2, 1, 1, 2, 3, 1, 1, 1, 1, 2, 2, 3, 2, 3]
+
 
 def queryToHash(x):
     sample = [0] * 26
@@ -10,9 +11,10 @@ def queryToHash(x):
     while i < len(x):
         sample[ord(x[i]) - ord('A')] += 1
         i += 2 if x[i] == 'Q' else 1
-    while sample[-1] == 0:sample.pop()
+    while sample[-1] == 0: sample.pop()
     return sample
     # the sample is a list
+
 
 def hashToValue(x):
     res = 0
@@ -20,37 +22,36 @@ def hashToValue(x):
         res += int(x[i]) * valueForLetter[i]
     return res
 
-def dfs(node,test):
+
+def dfs(node, test):
     global maxRes
-    if node.wordend:
-        val = hashToValue(node.hash)
-        if maxRes[0] < val:
-            maxRes = [val, hashToWord[node.hash]]
-    for freq,child in node.children.items():
-        if len(child.hash) > len(test):break
-        if freq <= test[len(child.hash)-1]:
-            dfs(child,test)
+    if maxRes[0] < node.val:
+        maxRes = [node.val, hashToWord[node.hash]]
+    for freq, child in node.children.items():
+        if len(child.hash) > len(test): break
+        if freq <= test[len(child.hash) - 1]:
+            dfs(child, test)
 
 
 class TreeNode(object):
     def __init__(self):
-        self.wordend = False
         self.hash = ''
+        self.val = 0
         self.children = defaultdict(TreeNode)
+
 
 
 # put origin words in list
 originDict = []
-with open('dictionary.txt','r') as f:
+with open('dictionary.txt', 'r') as f:
     for line in f:
         originDict.append(line.strip('\n').upper())
-        
+
 # create dictionary for hash to word
 hashToWord = {}
 for x in originDict:
     hashToWord[''.join(map(str, queryToHash(x)))] = x
     # use map to make list be string
-
 
 # create tree
 root = TreeNode()
@@ -60,10 +61,11 @@ for key in hashToWord:
         hash = node.hash
         node = node.children[int(key[i])]
         node.hash = hash + key[i]
-    node.wordend = True
+    node.val = hashToValue(node.hash)
 
 # input and dfs
-maxRes = [0,'']
-test = input('Please give 16 word:')
-dfs(root,queryToHash(test))
-print(maxRes)
+for i in range(10):
+    maxRes = [0, '']
+    test = input("Input:")
+    dfs(root, queryToHash(test))
+    print(maxRes)
