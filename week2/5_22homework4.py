e398c6f88b8841250c5649b6726ac732187d3502
Kyoko-N <65456318+Kyoko-N@users.noreply.github.com>
2020-05-27 22:18:47 +0900

    Update 5_22homework4.py

--- week2/5_22homework4.py
+++ week2/5_22homework4.py
@@ -1,3 +1,4 @@
+# k for key, v for value.
 class Node(object):
     def __init__(self, k, v):
         self.val = [k, v]
@@ -8,15 +9,19 @@ class Node(object):
 def get(k ,v, start, end):
     if cache_size >= 1:
         if not cache:
+            # initialization the double linked list.
             node = Node(k, v)
             cache[k] = node
             start = node
             end = node
         else:
+            # if the target_URL is already in cache and it is not the 'start'.
+            # [target_URL is not the latest url in double linked list]. 
             if k in cache and k != start.val[0]:
                 nodeToAdd, end = deleteNode(k, end)
                 cache[k] = nodeToAdd
                 start = addNewStart(nodeToAdd, start)
+            # if the target_URL not in cache.
             elif k not in cache:
                 cache[k] = Node(k, v)
                 start = addNewStart(cache[k], start)
@@ -24,8 +29,9 @@ def get(k ,v, start, end):
                     end = deleteNode(end.val[0], end)[1]
     return start, end
 
-# k != start[0]
+# k != start.val[0]
 def deleteNode(k, end):
+    # if k is the 'end'node, the oldest one.
     if k == end.val[0]:
         newEnd = end.right
         newEnd.left = None
@@ -35,6 +41,7 @@ def deleteNode(k, end):
         l,r = cache[k].left, cache[k].right
         l.right = r
         r.left = l.right
+    # delete the key-'k' from the chache and get new end.
     return cache.pop(k), end
 
 def addNewStart(newstart, start):
@@ -46,6 +53,7 @@ def addNewStart(newstart, start):
 
 
 cache = {}
+# cache's max size.
 cache_size = 3
 start, end = None, None
 start, end = get('a', 'A', start, end)
