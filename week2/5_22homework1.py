233850ddccf21f63e1ea66c7d3d13316e453365a
Kyoko-N <65456318+Kyoko-N@users.noreply.github.com>
2020-05-27 21:48:38 +0900

    Add files via upload

---
+++ week2/5_22homework1.py
@@ -0,0 +1,55 @@
+import numpy, time
+import matplotlib.pyplot as plt
+
+def multiple(A,B,C):
+    n = len(A)
+    if len(A) != n or len(B) != n: return 'Input Error'
+    for x in A + B:
+        if len(x) != n: return 'Input Error'
+
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                C[i][j] += A[i][k] * B[k][j]
+    return C
+
+x = []
+y = []
+
+for n in range(70):
+
+    a = numpy.zeros((n, n)) # Matrix A
+    b = numpy.zeros((n, n)) # Matrix B
+    c = numpy.zeros((n, n)) # Matrix C
+
+    # Initialize the matrices to some values.
+    for i in range(n):
+        for j in range(n):
+            a[i, j] = i * n + j
+            b[i, j] = j * n + i
+            c[i, j] = 0
+
+    begin = time.time()
+
+    c = multiple(a,b,c)
+
+    end = time.time()
+    print(n, "time: %.6f sec" % (end - begin))
+
+    # Print C for debugging. Comment out the print before measuring the execution time.
+    total = 0
+    for i in range(n):
+        for j in range(n):
+            # print c[i, j]
+            total += c[i, j]
+    # Print out the sum of all values in C.
+    # This should be 450 for N=3, 3680 for N=4, and 18250 for N=5.
+    print("sum: %.6f" % total)
+    x.append(n)
+    y.append(end - begin)
+
+
+
+plt.scatter(x, y, label='matrix multiplication')
+plt.legend()
+plt.show()
