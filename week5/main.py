572f376596ac439c7fd0d979cf080c68a1348f81
Kyoko-N <65456318+Kyoko-N@users.noreply.github.com>
2020-06-17 21:15:54 +0900

    Update main.py

--- week5/main.py
+++ week5/main.py
@@ -68,7 +68,7 @@ def isIntersect(a,b,c,d):
     x2,y2 = b
     x3,y3 = c
     x4,y4 = d
-    x = (y3*x4*x2 - y4*x3*x2 - y3*x4*x1 + y4*x3*x1 - y1*x2*x4 + y2*x1*x4 + y1*x2*x3 - y2*x1*x3) / x4*y2 - x4*y1 - x3*y2 + x3*y1 - x2*y4 + x2*y3 + x1*y4 - x1*y3
+    x = (y3*x4*x2 - y4*x3*x2 - y3*x4*x1 + y4*x3*x1 - y1*x2*x4 + y2*x1*x4 + y1*x2*x3 - y2*x1*x3) / (x4*y2 - x4*y1 - x3*y2 + x3*y1 - x2*y4 + x2*y3 + x1*y4 - x1*y3)
 
     return True if x1 < x < x2 else False
 
